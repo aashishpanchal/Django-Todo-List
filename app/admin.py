4c8a96306a04c5856d4f161d99cc2f1d56624e0b
AashishPanchal <aipanchal51@gmail.com>
2021-07-09 17:11:04 +0530

    remove all file and folder

--- app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import TODOModel
-
-# Register your models here.
-@admin.register(TODOModel)
-class TODOModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'Title', 'Description', 'Important', 'DateTime']
--- app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import TODOModel
-
-class TODOForm(forms.ModelForm):
-    class Meta:
-        model = TODOModel
-        fields = "__all__"
